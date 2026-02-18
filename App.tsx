06c3d39c239b73a5e48e56b302bee9892b78aba1
Lucas <lucas@example.com>
2026-02-18 14:08:23 -0300

    fix: Install missing gesture handler and reanimated dependencies

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { AuthProvider } from './src/context/AuthContext';
 import RootNavigation from './src/navigation';
