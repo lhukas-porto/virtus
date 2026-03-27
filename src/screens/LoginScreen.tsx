b58d90be2d3b76e7936f6b944873457559f3a558
Lucas <lucas@example.com>
2026-03-27 14:40:48 -0300

    chore: bump OTA version label to v1.0.19-OTA-ALARME

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -389,7 +389,7 @@ export const LoginScreen = () => {
 
             <View style={styles.footerBranding}>
                 <View style={{ alignItems: 'flex-end' }}>
-                    <Text style={[styles.brandingLabel, { color: 'red', fontFamily: theme.fonts.bold, opacity: 1 }]}>v1.0.19-FIX-ALARME</Text>
+                    <Text style={[styles.brandingLabel, { color: 'red', fontFamily: theme.fonts.bold, opacity: 1 }]}>v1.0.19-OTA-ALARME</Text>
                     <Text style={styles.brandingLabel}>Desenvolvido por</Text>
                 </View>
                 <Image
